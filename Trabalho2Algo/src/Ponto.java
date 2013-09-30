b9cecc03bbc3482244929a0cf2b01daad0f18237
rafaelmmont <rafaelmmont@gmail.com>
2013-09-30 19:39:02 -0300

    Teste

---
+++ Trabalho2Algo/src/Ponto.java
@@ -0,0 +1,22 @@
+import java.awt.Graphics;
+
+
+public class Ponto {
+
+	int x;
+    int y;
+	
+    public Ponto(int x, int y){
+    	this.x = x;
+    	this.y = y;
+    }
+
+	public void draw(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+}
